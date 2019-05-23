5761c9934071fb6f48661a2172090c9131684a38
yossaj <yossaj@icloud.com>
2019-05-23 13:44:36 +0100

    added joe

--- greet.js
+++ greet.js
@@ -1,3 +1,11 @@
+const joe = happy
+
+if( joe = happy){
+  helloWorld('joe')
+}else{
+  console.log('GO AWAY BIG GREEN MONSTER, GO AWAY!')
+}
+
 const helloWorld = (name = "World") => {
   return `Hello ${name}!`;
 }
